250d4634fb3a55738c57e22233599276a219df4f
chxlls <45939035+chxlls@users.noreply.github.com>
2019-10-28 21:41:09 +1300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+  
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+    props.help.aliases.forEach(alias => { 
+      bot.aliases.set(alias, props.help.name);
+  
+  });
+});
+})
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+  bot.user.setActivity(`In Development`);
+  bot.user.setStatus('online');
+
+  bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+    let prefix = botconfig.prefix
+    let messageArray = message.content.split(" ");
+    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let cmd = args.shift().toLowerCase();
+    let commandfile;
+
+    if (bot.commands.has(cmd)) {
+      commandfile = bot.commands.get(cmd);
+  } else if (bot.aliases.has(cmd)) {
+    commandfile = bot.commands.get(bot.aliases.get(cmd));
+  }
+  
+      if (!message.content.startsWith(prefix)) return;
+
+          
+  try {
+    commandfile.run(bot, message, args);
+  
+  } catch (e) {
+  }}
+  )})
+
+
+bot.login("Discord Token");
